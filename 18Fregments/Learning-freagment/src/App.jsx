fb7ac82cf89f1cc3d6ee9ab80c77a3effaf1f401
Anusha Saha <anushaofksms@gmail.com>
2023-12-18 13:55:46 +0530

    added css module

--- 18Fregments/Learning-freagment/src/App.jsx
+++ 18Fregments/Learning-freagment/src/App.jsx
@@ -3,15 +3,15 @@ import ErrorMessage from "./Components/ErrorMessage";
 import FoodItems from "./Components/FoodItems";
 
 function App() {
-  let foodItems = [];
-  // let foodItems = ["sabji", "Roti", "Salad", "Milk", "Veggies", "Ghee"];
+  // let foodItems = [];
+  let foodItems = ["Sabji", "Roti", "Salad", "Milk", "Veggies", "Ghee"];
 
   // if (foodItem.length === 0) {    it will hide all ui not good practise bcz we want to show header then error
   //   return <h3>I am still hungry</h3>;
   // }
   return (
     <div>
-      <div>Healthy Food</div>
+      <div className="food-heading">Healthy Food</div>
       <ErrorMessage items={foodItems}></ErrorMessage>
       <FoodItems items={foodItems}></FoodItems>
     </div>
--- 18Fregments/Learning-freagment/src/Components/Item.jsx
+++ 18Fregments/Learning-freagment/src/Components/Item.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import style from "./Item.module.css";
 
 function Item({ foodItem }) {
   // let { foodItem } = props;  this way also we can use else we can pass directly in arg
   return (
     <>
       {/* <li className="list-group-item">{props.foodItem}</li> if we are not using descructuring */}
-      <li className="list-group-item">{foodItem}</li>
+      {/* <li className="list-group-item kg-item"> if we are not using class from item.module.css
+        <span className="kg-apan">{foodItem}</span>
+      </li> */}
+      {/* list-group-item */}
+      <li className={`${style["kg-item"]} list-group-item`}>
+        <span className={style["kg-apan"]}>{foodItem}</span>
+      </li>
     </>
   );
 }
